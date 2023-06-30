e659a4ecb09a63ecb93c2f6a74c1c3b3d72b0e3c
felixj <841577533jyj@gmail.com>
2023-06-30 16:09:02 -0400

    Class24: use ResourceFilter as a shortCircuit

--- FilterDemo/FilterDemo.API/Filters/CtmResourceFilterAttribute.cs
+++ FilterDemo/FilterDemo.API/Filters/CtmResourceFilterAttribute.cs
@@ -1,16 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace FilterDemo.API.Filters;
 
-public class CtmResourceFilterAttribute: Attribute, IResourceFilter
+public class CtmResourceFilterAttribute : Attribute, IResourceFilter
 {
+    // Suitable for cache handling
+    // 短路器(shortCircuit)
+    // if `result`(context.result) is not null, it means that the short-cut is triggered.
+    // the model binding would not be executed
     public void OnResourceExecuting(ResourceExecutingContext context)
     {
-        throw new NotImplementedException();
+        // get the path of current request
+        var path = context.HttpContext.Request.Path;
+        // check if the path is in the `_dicCache`
+        if (_dicCache.ContainsKey(path))
+        {
+            // if the path is in the `_dicCache`, then return the result directly
+            context.Result = _dicCache[path] as ObjectResult;
+        }
     }
+
+    private static Dictionary<string, object> _dicCache = new();
 
     public void OnResourceExecuted(ResourceExecutedContext context)
     {
-        throw new NotImplementedException();
+        // how to judge the current visit has appeared before.
+        var path = context.HttpContext.Request.Path;
+        // store the result into the dict
+        _dicCache[path] = context.Result as ObjectResult;
     }
 }
